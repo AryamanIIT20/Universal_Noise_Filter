7ab5ec0d93c5aa06e72e9e4b95a81707b36b0baa
ARYAMAN VIKRAM TODI <75089457+AryamanIIT20@users.noreply.github.com>
2023-11-02 16:16:13 +0530

    Update get_UNF_kernel.m

--- 20EE38033_20EE38031_DIP_PROJECT/get_UNF_kernel.m
+++ 20EE38033_20EE38031_DIP_PROJECT/get_UNF_kernel.m
@@ -1,5 +1,5 @@
 function [h] = get_UNF_kernel(slice,nbd,s_s,s_r,s_i,s_j)
-slice_x = slice(nbd+1:3*nbd +1,nbd+1:3*nbd +1);
+slice_x = slice(nbd+1:3*nbd +1,nbd+1:3*nbd +1); 
 vec = sort(abs(slice_x(:)-slice_x(nbd+1,nbd+1)));
 road_x = sum(vec(1:5),"all");
 M = 2*nbd +1;
@@ -18,7 +18,7 @@ for m = nbd+1:nbd+M
         slice_y = slice(lr:ur,lc:uc);
         vec = sort(abs(slice_y(:)-slice_y(nbd+1,nbd+1)));
         road_y = sum(vec(1:5),"all"); % since first element is always zero
-        s_i = 25 + 30/(1+exp((road_y-60)));  % Modification by Sayan
+        s_i = 25 + 30/(1+exp((road_y-60)));  %dynamic estimation of radiometric threshold
         w_i = exp(-(road_y^2)/(2*s_i^2));
         w_r = exp(-((slice(m,n)-slice(m0,n0))^2)/(2*s_r^2));
         J = 1 - exp(-(((road_x+road_y)/2)^2)/(2*s_j^2));
