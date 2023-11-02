157e208a3c52af31de8392c4a0052e9a76b9430c
Sayan Tapadar <85694635+Syntrpd@users.noreply.github.com>
2023-11-02 16:26:05 +0530

    Update gaussian_noise.m

--- 20EE38033_20EE38031_DIP_PROJECT/gaussian_noise.m
+++ 20EE38033_20EE38031_DIP_PROJECT/gaussian_noise.m
@@ -1,4 +1,4 @@
-function [img_noisy] = gaussian_noise(img,stdev)
+function [img_noisy] = gaussian_noise(img,stdev) % Adds 0 mean, stdev standard deviation gaussian noise to image
 img = double(img)/255;
 img_noisy = img + stdev*randn(size(img));
 img_noisy = uint8(255*img_noisy);
